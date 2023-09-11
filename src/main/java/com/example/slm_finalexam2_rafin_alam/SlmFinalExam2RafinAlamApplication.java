76a749f39089d0f30950b7ad2e7988979ab65b67
rafin.alam1 <rafiniftaalam@gmail.com>
2023-09-11 10:34:58 +0200

    first commit

---
+++ src/main/java/com/example/slm_finalexam2_rafin_alam/SlmFinalExam2RafinAlamApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_finalexam2_rafin_alam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmFinalExam2RafinAlamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmFinalExam2RafinAlamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm_finalexam2_rafin_alam/SlmFinalExam2RafinAlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_finalexam2_rafin_alam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmFinalExam2RafinAlamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
